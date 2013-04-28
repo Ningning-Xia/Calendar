a152b4da3af116b3db5e2da92546ce52d3b83a8e
Fengjiao <fj2194@columbia.edu>
2013-04-28 00:42:29 -0400

    part of add friend jobs. further work is to handle request and show all the firends

---
+++ WebContent/addFriends.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ page import = "java.util.*" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link rel="StyleSheet" href="./styles/addFriends.css" type="text/css" />
+<title>Insert title here</title>
+</head>
+<body>
+<div id = "friends">
+
+</div>
+
+
+<div id = "search">
+<% ArrayList<ArrayList<String>> userinfo = new ArrayList<ArrayList<String>>(); %>
+<form action = "searchFriends" method = "post">
+<input type = "text" name = "username">
+<input type = "submit" name = "search" value = "search">
+</form>
+<% userinfo = (ArrayList<ArrayList<String>>)request.getAttribute("userinfo");%>
+
+
+<% if(userinfo != null){ %>
+Name:<%= userinfo.get(0).get(0) %>
+<br>
+Email:<%= userinfo.get(0).get(1) %>
+<input type = "checkbox" name = "friend" value = <%=userinfo.get(0).get(0) %>>
+<form action = "addFriend" method = "post">
+<input type = "submit" value = "Add Friend">
+</form>
+<% } %>
+
+
+</div>
+
+<div id = "requests">
+
+</div>
+
+</body>
+</html>
---
+++ src/servlet/SendEmails.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import model.Email;
+import management.EmailManage;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SendEmails
+ */
+public class SendEmails extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public Email email;
+	public EmailManage emanage;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SendEmails() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String fromAddr = "fj2194@columbia.edu";	
+		String toAddr = request.getParameter("email");
+		String subject = request.getParameter("subject");
+		String content = request.getParameter("body");
+		System.out.println(toAddr + " " + subject + " " + content + " ");
+		email = new Email(fromAddr, toAddr, subject,content);
+		emanage = new EmailManage();
+		emanage.sendEmail(email);
+		
+	}
+
+}
